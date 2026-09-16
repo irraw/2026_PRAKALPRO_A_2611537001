1e7caa48e13342b298f54a63fb4b1ec091665e87
irraw <irwwanobody@gmail.com>
2026-09-16 22:04:49 +0700

    Add files via upload

---
+++ Pekan 2/tugas2.py
@@ -0,0 +1,32 @@
+print ("=== SISTEM REGISTRASI PRAKTIKAN ALPRO 2026 ===")
+
+nama_7001 = input(str("Masukkan Nama Mahasiswa : "))
+jenis_kelamin_7001 = input ("Masukkan Jenis Kelamin (L/P) : ")
+umur_7001 = int(input("Masukkan Umur : "))
+skor_tes_awal_7001 = float(input("Masukkan Skor Tes Awal : "))
+
+print("\n=== DATA PRAKTIKAN & HASIL PEMERIKSAAN ===")
+
+alamat_7001 = """
+perumahan sukasari,
+Kec. sarolangun,
+Kab. sarolangun """
+id_token_sinyal = 100+3j
+
+print("Nama Mahasiswa :", nama_7001, "|", "Tipe :", type(nama_7001))
+print("Jenis Kelamin :", jenis_kelamin_7001, "|", "Tipe :", type(jenis_kelamin_7001))
+print("Alamat Domisili :", alamat_7001, "|", "Tipe :", type(alamat_7001))
+print("Umur :", umur_7001, "tahun", "|", "Tipe :", type(umur_7001))
+print("Skor Tes Awal :", skor_tes_awal_7001, "|", "Tipe :", type (skor_tes_awal_7001))
+print("ID Token Sinyal :", id_token_sinyal, "|", "Tipe :", type (id_token_sinyal))
+
+print("\n=== STATUS KELULUSAN PRAKTIKUM ===")
+
+batas_7001 = 75.0
+
+print("Batas Minimum Nilai:", batas_7001)
+if skor_tes_awal_7001 >= batas_7001:
+    hasil_7001 = True
+else:
+    hasil_7001 = False
+print("Apakah dinyatakan lulus?:", hasil_7001, "|", "Tipe :", type (hasil_7001))
